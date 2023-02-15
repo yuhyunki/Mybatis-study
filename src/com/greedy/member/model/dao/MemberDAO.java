5201f908b4c4b8258baad6f01bff823867259eab
Yuhyun Kim <christykim47@gmail.com>
2023-02-15 15:35:36 +0900

    finished 1st feature : register new member

--- src/com/greedy/member/model/dao/MemberDAO.java
+++ src/com/greedy/member/model/dao/MemberDAO.java
@@ -1,5 +1,14 @@
 package com.greedy.member.model.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.greedy.member.model.dto.MemberDTO;
+
 public class MemberDAO {
+
+	public int insertNewMember(SqlSession sqlSession, MemberDTO member) {
+
+		return sqlSession.insert("MemberMapper.insertMember", member);
+	}
 
 }
